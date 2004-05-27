ce5e1e755d252618e8c2fc48bd448991c53e59c1
Stuart Bishop <stuart@stuartbishop.net>
2004-05-27 02:11:08 +0000

    RAMSessionDataContainer for in-memory session data. Not ZEO compatible.

---
+++ __init__.py
@@ -0,0 +1,260 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+Session implementation using cookies
+
+$Id$
+"""
+import sha, time, string, random, hmac, logging, warnings, thread
+from UserDict import IterableUserDict
+from heapq import heapify, heappop
+
+from persistent import Persistent
+from zope.server.http.http_date import build_http_date
+from zope.interface import implements
+from zope.interface.common.mapping import IMapping
+from zope.component import ComponentLookupError
+from zope.app import zapi
+from BTrees.OOBTree import OOBTree
+from zope.app.utility.interfaces import ILocalUtility
+from zope.app.annotation.interfaces import IAttributeAnnotatable
+
+from interfaces import IBrowserIdManager, IBrowserId, ICookieBrowserIdManager, \
+                       ISessionDataContainer, ISession
+from zope.app.container.interfaces import IContained
+
+import ZODB
+import ZODB.MappingStorage
+
+cookieSafeTrans = string.maketrans("+/", "-.")
+
+def digestEncode(s):
+    """Encode SHA digest for cookie."""
+    return s.encode("base64")[:-2].translate(cookieSafeTrans)
+
+
+class BrowserId(str):
+    """A browser id"""
+    implements(IBrowserId)
+
+
+class CookieBrowserIdManager(Persistent):
+    """Session service implemented using cookies."""
+
+    implements(IBrowserIdManager, ICookieBrowserIdManager,
+               ILocalUtility, IAttributeAnnotatable,
+               )
+
+    __parent__ = __name__ = None
+
+    def __init__(self):
+        self.namespace = "zope3_cs_%x" % (int(time.time()) - 1000000000)
+        self.secret = "%.20f" % random.random()
+        self.cookieLifetime = None
+
+    def generateUniqueId(self):
+        """Generate a new, random, unique id."""
+        data = "%.20f%.20f%.20f" % (random.random(), time.time(), time.clock())
+        digest = sha.sha(data).digest()
+        s = digestEncode(digest)
+        # we store a HMAC of the random value together with it, which makes
+        # our session ids unforgeable.
+        mac = hmac.new(s, self.secret, digestmod=sha).digest()
+        return BrowserId(s + digestEncode(mac))
+
+    def getRequestId(self, request):
+        """Return the IBrowserId encoded in request or None if it's
+        non-existent."""
+        # If there is an id set on the response, use that but don't trust it.
+        # We need to check the response in case there has already been a new
+        # session created during the course of this request.
+        response_cookie = request.response.getCookie(self.namespace)
+        if response_cookie:
+            sid = response_cookie['value']
+        else:
+            sid = request.cookies.get(self.namespace)
+        if sid is None or len(sid) != 54:
+            return None
+        s, mac = sid[:27], sid[27:]
+        if (digestEncode(hmac.new(s, self.secret, digestmod=sha).digest())
+            != mac):
+            return None
+        else:
+            return BrowserId(sid)
+
+    def setRequestId(self, request, id):
+        """Set cookie with id on request."""
+        # XXX Currently, the path is the ApplicationURL. This is reasonable,
+        #     and will be adequate for most purposes.
+        #     A better path to use would be that of the folder that contains
+        #     the service-manager this service is registered within. However,
+        #     that would be expensive to look up on each request, and would
+        #     have to be altered to take virtual hosting into account.
+        #     Seeing as this service instance has a unique namespace for its
+        #     cookie, using ApplicationURL shouldn't be a problem.
+
+        if self.cookieLifetime is not None:
+            if self.cookieLifetime:
+                expires = build_http_date(time.time() + self.cookieLifetime)
+            else:
+                expires = 'Tue, 19 Jan 2038 00:00:00 GMT'
+            request.response.setCookie(
+                    self.namespace, id, expires=expires,
+                    path=request.getApplicationURL(path_only=True)
+                    )
+        else:
+            request.response.setCookie(
+                    self.namespace, id,
+                    path=request.getApplicationURL(path_only=True)
+                    )
+
+    def getBrowserId(self, request):
+        ''' See zope.app.interfaces.utilities.session.IBrowserIdManager '''
+        sid = self.getRequestId(request)
+        if sid is None:
+            sid = self.generateUniqueId()
+        self.setRequestId(request, sid)
+        return sid
+
+
+class PersistentSessionDataContainer(Persistent, IterableUserDict):
+    ''' A SessionDataContainer that stores data in the ZODB '''
+    __parent__ = __name__ = None
+
+    implements(ISessionDataContainer, ILocalUtility, IAttributeAnnotatable)
+
+    def __init__(self):
+        self.data = OOBTree()
+        self.sweepInterval = 5*60
+
+    def __getitem__(self, key):
+        rv = IterableUserDict.__getitem__(self, key)
+        now = time.time()
+        # Only update lastAccessTime once every few minutes, rather than
+        # every hit, to avoid ZODB bloat since this is being stored 
+        # persistently
+        if rv.lastAccessTime + self.sweepInterval < now:
+            rv.lastAccessTime = now
+            # XXX: When scheduler exists, this method should just schedule
+            # a sweep later since we are currently busy handling a request
+            # and may end up doing simultaneous sweeps
+            self.sweep()
+        return rv
+
+    def sweep(self):
+        ''' Clean out stale data '''
+        expire_time = time.time() - self.sweepInterval
+        heap = [(v.lastAccessTime, k) for k,v in self.data.items()]
+        heapify(heap)
+        while heap:
+            lastAccessTime, key = heappop(heap)
+            if lastAccessTime < expire_time:
+                del self.data[key]
+            else:
+                return
+
+_ram_session_storages = {}
+
+class RAMSessionDataContainer(PersistentSessionDataContainer):
+    ''' A SessionDataContainer that stores data in RAM. Currently session
+        data is not shared between Zope clients, so server affinity will
+        need to be maintained to use this in a ZEO cluster.
+    '''
+    def __init__(self):
+        self.sweepInterval = 5*60
+        self.key = sha.new(str(time.time() + random.random())).hexdigest()
+
+    _ram_storage = ZODB.MappingStorage.MappingStorage()
+    _conns = {}
+
+    def _getData(self):
+
+        # XXX: Nuke
+        if not hasattr(self, 'key'):
+            self.key = sha.new(str(time.time() + random.random())).hexdigest()
+
+        # Open a connection to _ram_storage per thread
+        tid = thread.get_ident()
+        if not self._conns.has_key(tid):
+            db = ZODB.DB(self._ram_storage)
+            self._conns[tid] = db.open()
+
+        root = self._conns[tid].root()
+        if not root.has_key(self.key):
+            root[self.key] = OOBTree()
+        return root[self.key]
+
+        logger = logging.getLogger('zope.app.session')
+        logger.error('Oops %r' % (_ram_session_storages.keys(),))
+        
+    data = property(_getData, None)
+
+
+class SessionData(Persistent, IterableUserDict):
+    ''' Mapping nodes in the ISessionDataContainer tree '''
+    implements(IMapping)
+
+    def __init__(self):
+        self.data = OOBTree()
+        self.lastAccessTime = time.time()
+
+
+class Session(IterableUserDict):
+    implements(ISession)
+    def __init__(self, data_manager, browser_id, product_id):
+        ''' See zope.app.interfaces.utilities.session.ISession '''
+        browser_id = str(browser_id)
+        product_id = str(product_id)
+        try:
+            data = data_manager[browser_id]
+        except KeyError:
+            data_manager[browser_id] = SessionData()
+            data_manager[browser_id][product_id] = SessionData()
+            self.data = data_manager[browser_id][product_id]
+        else:
+            try:
+                self.data = data[product_id]
+            except KeyError:
+                data[product_id] = SessionData()
+                self.data = data[product_id]
+
+# XXX: remove context arg
+def getSession(context, request, product_id, session_data_container=None):
+    ''' Retrieve an ISession. session_data_container defaults to 
+        an ISessionDataContainer utility registered with the name product_id
+
+        XXX: This method will probably be changed when we have an
+            Interaction or other object that combines context & request
+            into a single object.
+    '''
+    if session_data_container is None:
+        try:
+            dc = zapi.getUtility(ISessionDataContainer, product_id)
+        except ComponentLookupError:
+            warnings.warn(
+                    'Unable to find ISessionDataContainer named %s. '
+                    'Using default' % repr(product_id),
+                    RuntimeWarning
+                    )
+            dc = zapi.getUtility(ISessionDataContainer)
+    elif ISessionDataContainer.providedBy(session_data_container):
+        dc = session_data_container
+    else:
+        dc = zapi.getUtility(ISessionDataContainer, session_data_container)
+
+    bim = zapi.getUtility(IBrowserIdManager)
+    browser_id = bim.getBrowserId(request)
+    return Session(dc, browser_id, product_id)
+
+
---
+++ tests.py
@@ -0,0 +1,288 @@
+# -*- coding: ascii -*-
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+'''
+$Id$
+'''
+import unittest, doctest, time, rfc822
+from zope.app import zapi
+from zope.app.tests import ztapi
+from zope.app.tests import setup
+import zope.interface
+from zope.app.utility.interfaces import ILocalUtility
+from zope.app.utility import LocalUtilityService
+from zope.app.servicenames import Utilities
+from zope.app.annotation.interfaces import IAttributeAnnotatable
+
+from zope.app.session.interfaces import \
+        IBrowserId, IBrowserIdManager, ISession, ISessionDataContainer
+
+from zope.app.session import \
+        CookieBrowserIdManager, Session, SessionData, getSession, \
+        PersistentSessionDataContainer, RAMSessionDataContainer
+
+from zope.publisher.interfaces.http import IHTTPRequest
+from zope.publisher.http import HTTPRequest
+
+def test_CookieBrowserIdManager():
+    """
+    CookieBrowserIdManager.generateUniqueId should generate a unique
+    IBrowserId each time it is called
+
+    >>> bim = CookieBrowserIdManager()
+    >>> id1 = bim.generateUniqueId()
+    >>> id2 = bim.generateUniqueId()
+    >>> id1 != id2
+    True
+    >>> IBrowserId.providedBy(id1)
+    True
+    >>> IBrowserId.providedBy(id2)
+    True
+
+    CookieBrowserIdManager.getRequestId pulls the IBrowserId from an
+    IHTTPRequest, or returns None if there isn't one stored in it.
+    Because cookies cannnot be trusted, we confirm that they are not forged,
+    returning None if we have a corrupt or forged browser id.
+    
+    >>> request = HTTPRequest(None, None, {}, None)
+    >>> bim.getRequestId(request) is None
+    True
+    >>> bim.setRequestId(request, id1)
+    >>> bim.getRequestId(request) == id1
+    True
+    >>> bim.setRequestId(request, 'invalid_id')
+    >>> bim.getRequestId(request) is None
+    True
+
+    Make sure that the same browser id is extracted from a cookie in
+    request (sent from the browser) and a cookie in request.response
+    (set during this transaction)
+
+    >>> request2 = HTTPRequest(None, None, {}, None)
+    >>> request2._cookies = request.response._cookies
+    >>> bim.getRequestId(request) == bim.getRequestId(request2)
+    True
+
+    CookieBrowserIdManager.getBrowserId pulls the IBrowserId from an
+    IHTTPRequest, or generates a new one and returns it after storing
+    it in the request.
+
+    >>> id3 = bim.getBrowserId(request)
+    >>> id4 = bim.getBrowserId(request)
+    >>> str(id3) == str(id4)
+    True
+    >>> id3 == id4
+    True
+
+    Confirm the path of the cookie is correct. The value being tested
+    for here will eventually change - it should be the URL to the
+    site containing the CookieBrowserIdManager
+
+    >>> cookie = request.response.getCookie(bim.namespace)
+    >>> cookie['path'] == request.getApplicationURL(path_only=True)
+    True
+
+    Confirm the expiry time of the cookie is correct.
+    Default is no expires.
+
+    >>> cookie.has_key('expires')
+    False
+
+    Expiry time of 0 means never (well - close enough)
+
+    >>> bim.cookieLifetime = 0
+    >>> request = HTTPRequest(None, None, {}, None)
+    >>> bid = bim.getBrowserId(request)
+    >>> cookie = request.response.getCookie(bim.namespace)
+    >>> cookie['expires']
+    'Tue, 19 Jan 2038 00:00:00 GMT'
+
+    >>> bim.cookieLifetime = 3600
+    >>> request = HTTPRequest(None, None, {}, None)
+    >>> bid = bim.getBrowserId(request)
+    >>> cookie = request.response.getCookie(bim.namespace)
+    >>> expires = time.mktime(rfc822.parsedate(cookie['expires']))
+    >>> expires > time.mktime(time.gmtime()) + 55*60
+    True
+    """
+
+def test_PersistentSessionDataContainer():
+    """
+    Ensure mapping interface is working as expected
+
+    >>> sdc = PersistentSessionDataContainer()
+    >>> sdc['a']
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in ?
+    File "/usr/python-2.3.3/lib/python2.3/UserDict.py", line 19, in __getitem__
+        def __getitem__(self, key): return self.data[key]
+    KeyError: 'a'
+    >>> sdc['a'] = SessionData()
+    >>> pdict = SessionData()
+    >>> sdc['a'] = pdict
+    >>> id(pdict) == id(sdc['a'])
+    True
+    >>> del sdc['a']
+    >>> sdc['a']
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in ?
+    File "/usr/python-2.3.3/lib/python2.3/UserDict.py", line 19, in __getitem__
+        def __getitem__(self, key): return self.data[key]
+    KeyError: 'a'
+    >>> del sdc['a']
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in ?
+    File "/usr/python-2.3.3/lib/python2.3/UserDict.py", line 21, in __delitem__
+        def __delitem__(self, key): del self.data[key]
+    KeyError: 'a'
+
+    Make sure stale data is removed
+
+    >>> sdc.sweepInterval = 60
+    >>> sdc[1], sdc[2] = sd1, sd2 = SessionData(), SessionData()
+    >>> ignore = sdc[1], sdc[2]
+    >>> sd1.lastAccessTime = sd1.lastAccessTime - 62
+    >>> sd2.lastAccessTime = sd2.lastAccessTime - 62
+    >>> ignore = sdc[1]
+    >>> sdc.get(2, 'stale')
+    'stale'
+    """
+
+def test_RAMSessionDataContainer(self):
+    pass
+test_RAMSessionDataContainer.__doc__ = \
+        test_PersistentSessionDataContainer.__doc__.replace(
+            'PersistentSessionDataContainer', 'RAMSessionDataContainer'
+            )
+
+def test_Session():
+    """
+    >>> data_container = PersistentSessionDataContainer()
+    >>> session = Session(data_container, 'browser id', 'zopeproducts.foo')
+    >>> session['color']
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in ?
+    File "zope/app/utilities/session.py", line 157, in __getitem__
+        return self._data[key]
+    KeyError: 'color'
+    >>> session['color'] = 'red'
+    >>> session['color']
+    'red'
+
+    And make sure no namespace conflicts...
+
+    >>> session2 = Session(data_container, 'browser id', 'zopeproducts.bar')
+    >>> session2['color'] = 'blue'
+    >>> session['color']
+    'red'
+    >>> session2['color']
+    'blue'
+
+    Test the rest of the dictionary interface...
+
+    >>> 'foo' in session
+    False
+    >>> 'color' in session
+    True
+    >>> session.get('size', 'missing')
+    'missing'
+    >>> session.get('color', 'missing')
+    'red'
+    >>> list(session.keys())
+    ['color']
+    >>> list(session.values())
+    ['red']
+    >>> list(session.items())
+    [('color', 'red')]
+    >>> len(session)
+    1
+    >>> [x for x in session]
+    ['color']
+    >>> del session['color']
+    >>> session.get('color') is None
+    True
+    """
+
+
+def test_localutilities():
+    """
+    Setup a placeful environment with a IBrowserIdManager
+    and ISessionDataContainer
+
+    >>> root = setup.placefulSetUp(site=True)
+    >>> setup.createStandardServices(root)
+    >>> sm = setup.createServiceManager(root, True)
+    >>> us = setup.addService(sm, Utilities, LocalUtilityService())
+    >>> idmanager = CookieBrowserIdManager()
+    >>> zope.interface.directlyProvides(idmanager,
+    ...                                 IAttributeAnnotatable, ILocalUtility)
+    >>> bim = setup.addUtility(
+    ...     sm, '', IBrowserIdManager, idmanager, 'test')
+    >>> pdc = PersistentSessionDataContainer()
+    >>> zope.interface.directlyProvides(pdc,
+    ...                                 IAttributeAnnotatable, ILocalUtility)
+    >>> sdc = setup.addUtility(sm, 'persistent', ISessionDataContainer, pdc)
+    >>> sdc = setup.addUtility(sm, 'products.foo',ISessionDataContainer, pdc)
+    >>> sdc = setup.addUtility(sm, 'products.bar', ISessionDataContainer, pdc)
+    >>> request = HTTPRequest(None, None, {}, None)
+   
+    Make sure we can access utilities
+
+    >>> sdc = zapi.getUtility(ISessionDataContainer, 'persistent',
+    ...                       context=root)
+    >>> bim = zapi.getUtility(IBrowserIdManager, context=root)
+
+    Make sure getSession works
+
+    >>> session1 = getSession(root, request, 'products.foo')
+    >>> session2 = getSession(root, request, 'products.bar', 'persistent')
+    >>> session3 = getSession(root, request, 'products.baz', pdc)
+    >>> session4 = getSession(root, request, 'products.foo')
+
+    Make sure it returned sane values
+
+    >>> ISession.providedBy(session1)
+    True
+    >>> ISession.providedBy(session2)
+    True
+    >>> ISession.providedBy(session3)
+    True
+
+    Make sure that product_ids don't share a namespace, except when they should
+
+    >>> session1['color'] = 'red'
+    >>> session2['color'] = 'blue'
+    >>> session1['color']
+    'red'
+    >>> session2['color']
+    'blue'
+    >>> session4['color']
+    'red'
+
+    >>> setup.placefulTearDown()
+    >>> 'Thats all folks!'
+    'Thats all folks!'
+    """
+
+def test_suite():
+    return unittest.TestSuite((
+        doctest.DocTestSuite(),
+        ))
+
+if __name__ == '__main__':
+    unittest.main()
+
+# vim: set filetype=python ts=4 sw=4 et si
+
+
