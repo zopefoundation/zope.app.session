36ecb39456d2de0f45f28fb9804d385ef14e91bb
Michael Howitz <mh@gocept.com>
2023-02-10 10:21:54 +0100

    Preparing release 5.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 
 setup(name='zope.app.session',
-      version='5.0.dev0',
+      version='5.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope session',
