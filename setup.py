a40e8be390f34972e74d54841cd6dcbefb37d350
Michael Howitz <m.howitz@minddistrict.com>
2025-04-23 17:19:55 +0200

    Update Python version support. (#7)
    * Drop support for Python 3.7, 3.8.
    
    * Add support for Python 3.12, 3.13.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2006 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -26,7 +26,7 @@ def read(*rnames):
 
 setup(name='zope.app.session',
       version='5.1.dev0',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.dev',
       description='Zope session',
       long_description=(
@@ -36,7 +36,7 @@ setup(name='zope.app.session',
           + '\n\n' +
           read('CHANGES.rst')
       ),
-      license='ZPL 2.1',
+      license='ZPL-2.1',
       keywords="zope3 session",
       classifiers=[
           'Development Status :: 5 - Production/Stable',
@@ -45,11 +45,11 @@ setup(name='zope.app.session',
           'License :: OSI Approved :: Zope Public License',
           'Programming Language :: Python',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: 3.10',
           'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: Implementation :: PyPy',
           'Natural Language :: English',
@@ -61,7 +61,7 @@ setup(name='zope.app.session',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['zope', 'zope.app'],
-      python_requires='>=3.7',
+      python_requires='>=3.9',
       extras_require={
           'test': [
               'transaction',
